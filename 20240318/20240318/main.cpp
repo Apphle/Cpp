087c9f9d68ecc948a552bc9c93b03ca111f2db0d
Apphle <125525800+Apphle@users.noreply.github.com>
2024-03-18 11:24:49 +0900

    Add files via upload

---
+++ 20240318/20240318/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+using namespace std;
+
+#define WIDTH 9
+#define HEIGHT 8
+
+int main()
+{
+	/*
+	int list[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	for (int i : list)
+	{
+		cout << i << " ";
+	}
+	cout << endl;
+	return 0;
+	///////////
+	int list[10];
+	int max;
+
+	for (int& elem : list)
+	{
+		elem = rand() % 100 + 1;
+		cout << elem << " ";
+	}
+	cout << endl;
+	max = list[0];
+	for (auto& elem : list)
+	{
+		if (elem > max)
+		{
+			max = elem;
+		}
+	}
+	cout << "�ִ밪 = " << max << endl;
+	return 0;
+	////////////////
+	int table[HEIGHT][WIDTH];
+	int r, c;
+
+	for (r = 0; r < HEIGHT; r++)
+	{
+		for (c = 0; c < WIDTH; c++)
+		{
+			table[r][c] = (r + 1) * (c + 1);
+		}
+	}
+
+	for (r = 0; r < HEIGHT; r++)
+	{
+		for (c = 0; c < WIDTH; c++)
+		{
+			cout << table[r][c] << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+	*/
+}
